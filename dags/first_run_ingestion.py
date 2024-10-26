1b3878c0bc41bd514c3cebb1958cf9c5d0e11160
Gabriel-Philot <bilphilot@gmail.com>
2024-10-26 19:37:13 -0300

    api_task_pod

--- dags/first_run_ingestion.py
+++ dags/first_run_ingestion.py
@@ -98,7 +98,6 @@ start = DummyOperator(task_id='start', dag=dag)
     # namespace="processor",
 ingestion = KubernetesPodOperator(
     task_id="tastk_ingestion_bronze_table",
-    name="brewery-ingestion",
     is_delete_operator_pod=True,
     namespace="orchestrator",
     pod_template_file=f"{DAGS_FOLDER_PATH}/api_teste_to_minio.yaml",
