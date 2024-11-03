2ec5eb9de42e3ff2ce1060df262ce985912718f7
Gabriel-Philot <bilphilot@gmail.com>
2024-11-03 19:08:47 -0300

    first_try_image_ingestion_old_dag

--- dags/ingestion-validation.py
+++ dags/ingestion-validation.py
@@ -4,77 +4,99 @@ using the taskflow API, KubernetesPodOperator and Dataset
 
 """
 
-from datetime import timedelta
-from os import getenv, path
-
-# [START import_module]
-# The DAG object; we'll need this to instantiate a DAG
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
-# from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor, S3KeysUnchangedSensor
+# TODO define libraries and imports
+from airflow import Dataset
+from airflow.decorators import dag, task, task_group
+from airflow.utils.task_group import TaskGroup
+from airflow.models.baseoperator import chain
+from datetime import datetime, timedelta
+from os import path
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
 
-from airflow.utils.dates import days_ago
-
-# [END import_module]
 
 DAGS_FOLDER_PATH = path.dirname(__file__)
 
-# [START default_args]
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
+# TODO DAG and task defaults
 default_args = {
-    "owner": "GabrielPhilot",
+    "owner": "user",
     "depends_on_past": False,
-    "start_date": days_ago(1),
-    "email": ["bilphilot@gmail.com"],
     "email_on_failure": False,
     "email_on_retry": False,
     "max_active_runs": 1,
     "retries": 1,
-    "retry_delay": timedelta(1),
+    "retry_delay": timedelta(minutes=5),
 }
-# [END default_args]
 
-# [START instantiate_dag]
 
-# MUDAR NOME DEPOIS
-dag = DAG(
-    "ingestion-validation",
+
+# TODO DAG and task defaults
+@dag(
+    dag_id="brewapi-ingestion-validation-minio",
     default_args=default_args,
-    schedule_interval="@once",
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
     tags=["spark", "kubernetes", "s3", "sensor", "minio", "bronze", "silver"],
 )
-# [END instantiate_dag]
+
 
 # [START set_tasks]
 
-start = DummyOperator(task_id='start', dag=dag)
+
+def brewapi_ingestion_validation_minio():
+    """Main DAG for Berewery ingestion and validation"""
+
+    @task_group(group_id='ingestion')
+    def ingestion_group():
+        @task
+        def start_ingestion():
+            print("Starting the ingestion part of the DAG")
 
 
-ingestion = KubernetesPodOperator(
-    task_id="tastk_ingestion_bronze_table",
-    name="api-test-pod2",
-    is_delete_operator_pod=True,
-    namespace="orchestrator",
-    pod_template_file=f"{DAGS_FOLDER_PATH}/python_jobs/testev0.yaml",
-    kubernetes_conn_id="kubernetes_default",
-    in_cluster=True,
-    get_logs=True,
-    do_xcom_push=True,
-    dag=dag,
-)
+        ingestion = KubernetesPodOperator(
+            task_id="brewapi-ingestion-minio",
+            name="api-test-pod2",
+            is_delete_operator_pod=True,
+            namespace="orchestrator",
+            pod_template_file=f"{DAGS_FOLDER_PATH}/python_jobs/brewapi_ingestion.yaml",
+            kubernetes_conn_id="kubernetes_default",
+            in_cluster=True,
+            get_logs=True,
+            do_xcom_push=True,
+            dag=dag,
+        )
 
 
-end = DummyOperator(task_id='end', dag=dag)
-# [END set_tasks]
-# [START task_sequence]
-(
-    start
-    >> ingestion
-    >> end
-)
-# [END task_sequence]
+        @task
+        def end_ingestion():
+            print("Ending the ingestion part of the DAG")
+
+        chain(
+            start_ingestion(),
+            ingestion,
+            end_ingestion()
+        )
+
+    @task_group(group_id='validation')
+    def validation_group():
+        @task
+        def start_validation():
+            print("Starting the validation part of the DAG")
+
+        @task
+        def end_validation():
+            print("Ending the validation part of the DAG")
+        
+        chain(
+            start_validation(),
+            end_validation()
+        )
+
+    ingestion = ingestion_group()
+    validation = validation_group()
+
+    ingestion >> validation
+    
+dag = brewapi_ingestion_validation_minio()
