8105613c478a5747e95d9b2bf66dcd84dfeeef10
Gabriel-Philot <bilphilot@gmail.com>
2024-11-06 09:10:13 -0300

    ingestion_sensor_pod

--- images/spark_brewery/resources/conf_paths.py
+++ images/spark_brewery/resources/conf_paths.py
@@ -1,5 +1,5 @@
 # paths to lakehouse
 
-bronze_layer_path = "s3a://lakehouse/bronze_layer/"
-silver_layer_path = "s3a://lakehouse/silver_layer/"
-gold_layer_path = "s3a://lakehouse/gold_layer/"
+bronze_layer_path = "s3a://lakehouse/bronze/"
+silver_layer_path = "s3a://lakehouse/silver/"
+gold_layer_path = "s3a://lakehouse/gold/"
